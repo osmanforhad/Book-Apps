1d2f85711c4af056c6ac723a1b999a6e77454fa7
osmanforhad <osmanforhad93@gmail.com>
2020-04-05 21:29:37 +0600

    modifi customitemclickListener

--- app/src/main/java/com/osman/bookapp/CustomItemClickListener.java
+++ app/src/main/java/com/osman/bookapp/CustomItemClickListener.java
@@ -1,6 +1,10 @@
 package com.osman.bookapp;
 
+import android.view.View;
+
 public interface CustomItemClickListener {
 
+/* Writing Method for on item click **/
+    public void onItemClick(View v,int position);
 
 }//end of the interface
